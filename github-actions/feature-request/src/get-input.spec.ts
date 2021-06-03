8467fd69f068914f8f25e10b1a80c183ee886de4
Minko Gechev <mgechev@gmail.com>
2021-06-03 09:15:48 -0700

    fix: proper parsing of input parameters (#96)
    The input parser was not processing properly negative numbers, which was
    causing the bot to exit early.

---
+++ github-actions/feature-request/src/get-input.spec.ts
@@ -0,0 +1,58 @@
+import { getInputValue } from './get-input';
+
+describe('getInputValue', () => {
+  it('should parse strings', () => {
+    const result = getInputValue('foo', {
+      getInput() {
+        return 'a';
+      },
+    });
+    expect(result).toBe('a');
+  });
+
+  it('should parse boolean values', () => {
+    const trueResult = getInputValue('foo', {
+      getInput() {
+        return 'true';
+      },
+    });
+    expect(trueResult).toBe(true);
+
+    const falseResult = getInputValue('foo', {
+      getInput() {
+        return 'false';
+      },
+    });
+    expect(falseResult).toBe(false);
+  });
+
+  it('should parse numbers', () => {
+    const negative = getInputValue('foo', {
+      getInput() {
+        return '-1';
+      },
+    });
+    expect(negative).toBe(-1);
+
+    const positive = getInputValue('foo', {
+      getInput() {
+        return '42';
+      },
+    });
+    expect(positive).toBe(42);
+
+    const decimal = getInputValue('foo', {
+      getInput() {
+        return '42.42';
+      },
+    });
+    expect(decimal).toBe(42.42);
+
+    const decimalNegative = getInputValue('foo', {
+      getInput() {
+        return '-42.42';
+      },
+    });
+    expect(decimalNegative).toBe(-42.42);
+  });
+});
