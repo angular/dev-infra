8052beedad44b89b0e75f44209025c6e1417266c
mgechev <mgechev@gmail.com>
2021-06-30 12:56:56 -0700

    fix: log user login rather than user object

--- github-actions/feature-request/src/action.ts
+++ github-actions/feature-request/src/action.ts
@@ -87,10 +87,10 @@ const processIssue = async (githubAPI: GitHubAPI, githubIssue: GitHubIssueAPI, c
 
   log(
     `Started processing issue #${issue.number}:\n` +
-    `  Title:    ${issue.title}\n` +
-    `  Author:   ${issue.author}\n` +
-    `  Votes:    ${issue.reactions['+1']}\n` +
-    `  Comments: ${issue.numComments}`
+      `  Title:    ${issue.title}\n` +
+      `  Author:   ${issue.author.name}\n` +
+      `  Votes:    ${issue.reactions['+1']}\n` +
+      `  Comments: ${issue.numComments}`,
   );
 
   // Issues opened by team members bypass the process.
@@ -154,7 +154,7 @@ const processIssue = async (githubAPI: GitHubAPI, githubIssue: GitHubIssueAPI, c
     log(`Insufficient votes for feature request #${issue.number}`);
     const actions = [
       githubIssue.postComment(comment(CommentMarkers.Close, config.closeComment)),
-      githubIssue.addLabel(config.insufficientVotesLabel)
+      githubIssue.addLabel(config.insufficientVotesLabel),
     ];
     if (config.closeWhenNoSufficientVotes) {
       actions.push(githubIssue.close());
