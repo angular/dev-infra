bfeb3a32d0efd9f393694e18fc01fbb9840c83fd
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-08-03 15:15:45 +0200

    test: add test for active release train determination (#119)
    Sets up unit tests for the active release train determination. We
    transitively tested this through the merge tool testing, but rather
    in an integration-test manner, than in actual unit tests. This
    commit sets up actual unit tests so that we can test new logic
    like the "configured main branch" or "pagination for Github branches".
    
    PR Close #119

---
+++ ng-dev/utils/testing/github-interception.ts
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {buildGithubPaginationResponseHeader} from './github-pagination-header';
+import {ParsedUrlQuery} from 'querystring';
+import * as nock from 'nock';
+
+/**
+ * Fakes a Github server response with pagination.
+ *
+ * @see https://docs.github.com/en/rest/guides/traversing-with-pagination.
+ * @example https://docs.github.com/en/rest/reference/repos#list-branches.
+ */
+export function fakeGithubPaginationResponse(url: string, data: unknown[]) {
+  // For each data entry, create its own API page so that pagination is required
+  // to resolve all data.
+  for (let index = 0; index < data.length; index++) {
+    // Pages start with `1` as per the Github API specification.
+    const pageNum = index + 1;
+    const entry = data[index];
+    const linkHeader = buildGithubPaginationResponseHeader(data.length, pageNum, url);
+
+    // For the first page, either `?page=1` needs to be set, or no `page` should be specified.
+    const queryMatch =
+      pageNum === 1
+        ? (params: ParsedUrlQuery) => params.page === '1' || params.page === undefined
+        : {page: pageNum};
+
+    nock(url).get('').query(queryMatch).reply(200, [entry], {
+      link: linkHeader,
+    });
+  }
+}
