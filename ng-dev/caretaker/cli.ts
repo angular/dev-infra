eb2f08a1a2d8b713c816d962b6c9383f7a0e70c7
Joey Perrott <josephperrott@gmail.com>
2021-08-12 09:39:26 -0700

    feat(ng-dev): fail gracefully with message if caretaker command is used without a caretaker config provided
    If a usage of ng-dev caretaker <command> occurs without the a configuration being provided for
    the caretaker command, exit early with instructional message.

--- ng-dev/caretaker/cli.ts
+++ ng-dev/caretaker/cli.ts
@@ -6,11 +6,25 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Argv} from 'yargs';
+import {info} from 'console';
+import {Arguments, Argv} from 'yargs';
 import {CheckModule} from './check/cli';
+import {getCaretakerConfig} from './config';
 import {HandoffModule} from './handoff/cli';
 
 /** Build the parser for the caretaker commands. */
 export function buildCaretakerParser(yargs: Argv) {
-  return yargs.command(CheckModule).command(HandoffModule);
+  return yargs
+    .middleware(caretakerCommandCanRun, false)
+    .command(CheckModule)
+    .command(HandoffModule);
+}
+
+function caretakerCommandCanRun(argv: Arguments) {
+  const config = getCaretakerConfig();
+  if (config.caretaker === undefined) {
+    info('The `caretaker` command is not enabled in this repository.');
+    info(`   To enable it, provide a caretaker config in the repository's .ng-dev/ directory`);
+    process.exit(1);
+  }
 }
