c703b348967fe07a6c17a1e24184384b16e3a922
Joey Perrott <josephperrott@gmail.com>
2021-08-18 16:46:35 -0700

    fix(github-actions): refactor and update rerun-circle command as circleci's API has updated

--- github-actions/slash-commands/lib/commands/rerun-circleci.ts
+++ github-actions/slash-commands/lib/commands/rerun-circleci.ts
@@ -2,14 +2,14 @@ import * as core from '@actions/core';
 import {Octokit} from '@octokit/rest';
 import {context} from '@actions/github';
 import {getAuthTokenFor, ANGULAR_ROBOT} from '../../../utils';
-import fetch from 'node-fetch';
+import fetch, {HeaderInit, RequestInit} from 'node-fetch';
 import {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods';
 
 export async function rerunCircleCi() {
-  const circleToken = core.getInput('circleci-token');
-  const token = await getAuthTokenFor(ANGULAR_ROBOT);
-  // Create authenticated Github client.
-  const github = new Octokit({auth: token});
+  /** Authenticated CircleCi client */
+  const circleci = new CircleCiClient();
+  /** Authenticated Github client. */
+  const github = new Octokit({auth: await getAuthTokenFor(ANGULAR_ROBOT)});
   /** The pull request for the action currently running. */
   const {data: pullRequest} = await github.pulls.get({
     ...context.repo,
@@ -17,21 +17,8 @@ export async function rerunCircleCi() {
   });
 
   try {
-    const workflowId = await getCircleCiWorkflowIdForPullRequest(pullRequest, github, circleToken);
-    const rerunUrl = `https://circleci.com/api/v2/workflow/${workflowId}/rerun`;
-    // Attempt to restart the rerun.
-    const response = await (
-      await fetch(rerunUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Circle-Token': circleToken,
-        },
-        body: JSON.stringify({from_failed: true}),
-      })
-    ).json();
-
-    assertNoErrorsInCircleCiResponse(response);
+    const workflowId = await getCircleCiWorkflowIdForPullRequest(pullRequest, github, circleci);
+    await circleci.post(`workflow/${workflowId}/rerun`, {from_failed: true});
   } catch (err) {
     // If the rerun attempt failed comment on the issue informing the requestor.
     await github.issues.createComment({
@@ -52,7 +39,7 @@ export async function rerunCircleCi() {
 async function getCircleCiWorkflowIdForPullRequest(
   pullRequest: RestEndpointMethodTypes['pulls']['get']['response']['data'],
   github: Octokit,
-  circleCiToken: string,
+  circleci: CircleCiClient,
 ) {
   const {owner, repo} = context.repo;
   /** The list of statuses for the latest ref of the PR. */
@@ -63,49 +50,70 @@ async function getCircleCiWorkflowIdForPullRequest(
       ref: pullRequest.head.ref,
     })
   ).data;
-  /** The target url of the discovered CircleCI status. */
-  let targetUrl = statuses.find((status) => status.context.startsWith('ci/circleci:'))?.target_url;
+  /**
+   * The target url of the discovered CircleCI status, reversed to search the latest statuses first.
+   */
+  let targetUrl = statuses
+    .reverse()
+    .find((status) => status.context.startsWith('ci/circleci:'))?.target_url;
 
   if (targetUrl === undefined) {
     throw Error('No status for a CircleCI workflow was found on the pull request to be rerun.');
   }
 
   /** Results of the regex to select the job ID of the job which the status represents. */
-  const jobIdMatcher = targetUrl.match(`https://circleci.com/gh/${owner}/${repo}/(\d+)\?`);
+  const jobIdMatcher = targetUrl.match(`https://circleci.com/gh/${owner}/${repo}/(\\d+)`);
 
   if (jobIdMatcher === null) {
     throw Error('Unable to determine the job ID for the CircleCI job creating the status');
   }
 
   /** The job ID. */
-  const job = jobIdMatcher[0];
-  /** The full url of the API request to CircleCI. */
-  const url = `https://circleci.com/api/v2/project/gh/${owner}/${repo}/job/${job}`;
+  const job = jobIdMatcher[1];
   /** The API response from the CircleCI request. */
-  const response = await (
-    await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Circle-Token': circleCiToken,
-      },
-    })
-  ).json();
+  const result = await circleci.get(`project/gh/${owner}/${repo}/job/${job}`);
 
-  assertNoErrorsInCircleCiResponse(response);
-
-  return response.latest_workflow.id;
+  return result.latest_workflow.id;
 }
 
-/**
- * Checks the provided response from CircleCI's API to determine if it is an error message.
- *
- * Properly handled failures in the CircleCI requests are returned with an HTTP response code of 200
- * and json response with a `:message` key mapping to the failure message.  If `:message` is not
- * defined, the API request was successful.
- */
-function assertNoErrorsInCircleCiResponse(response: any) {
-  if (response[':message'] !== undefined) {
-    throw Error(response[':message']);
+class CircleCiClient {
+  /** Headers to include in all HTTP requests. */
+  private headers: HeaderInit = {
+    'Content-Type': 'application/json',
+    'Circle-Token': core.getInput('circleci-token', {required: true}),
+  };
+
+  /** Perform a GET request against the CircleCI api. */
+  async get<T = any>(path: string): Promise<T> {
+    return this.request<T>(path, 'GET');
+  }
+
+  /** Perform a POST request against the CircleCI api. */
+  async post<T = any>(path: string, body: Object): Promise<T> {
+    return this.request<T>(path, 'POST', body);
+  }
+
+  /** Perform a request against the CircleCI api. */
+  private async request<T>(path: string, method: 'GET' | 'POST', body: Object = {}): Promise<T> {
+    const url = `https://circleci.com/api/v2/${path}`;
+
+    const requestInit: RequestInit = {
+      method,
+      headers: this.headers,
+    };
+
+    if (method !== 'GET') {
+      requestInit['body'] = JSON.stringify(body);
+    }
+
+    const response = await fetch(url, requestInit);
+    const responseJson = await response.json();
+
+    if (!response.ok) {
+      const message = responseJson['message'] || 'Unknown error';
+      throw Error(`Error code: ${response.status}  Message: ${message}`);
+    }
+
+    return responseJson as T;
   }
 }
