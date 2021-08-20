3f4646c252bc54b2ae93b4e322ebde9030de30b2
Joey Perrott <josephperrott@gmail.com>
2021-08-19 18:48:28 -0700

    build: use no targetting labels (#137)
    PR Close #137

--- .ng-dev/merge.ts
+++ .ng-dev/merge.ts
@@ -1,19 +1,11 @@
-import {DevInfraMergeConfig} from '../ng-dev/pr/merge/config';
+import {MergeConfig} from '../ng-dev/pr/merge/config';
 
 /** Configuration for merging pull requests into the repo. */
-export const merge: DevInfraMergeConfig['merge'] = async (api) => {
-  return {
-    githubApiMerge: false,
-    claSignedLabel: 'cla: yes',
-    mergeReadyLabel: 'action: merge',
-    caretakerNoteLabel: 'merge note',
-    commitMessageFixupLabel: 'needs commit fixup',
-    labels: [
-      {
-        // Since only one branch is merged into for the repository, we use our merge label as a marker.
-        pattern: 'action: merge',
-        branches: ['main'],
-      },
-    ],
-  };
+export const merge: MergeConfig = {
+  githubApiMerge: false,
+  claSignedLabel: 'cla: yes',
+  mergeReadyLabel: 'action: merge',
+  caretakerNoteLabel: 'merge note',
+  commitMessageFixupLabel: 'needs commit fixup',
+  noTargetLabeling: true,
 };
