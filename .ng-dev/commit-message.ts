96a612efed1730c632ccd52895fd332986d5bd4d
Joey Perrott <josephperrott@gmail.com>
2021-08-23 11:07:36 -0700

    build: establish commit message requirements for the repository

---
+++ .ng-dev/commit-message.ts
@@ -0,0 +1,31 @@
+import {CommitMessageConfig} from '../ng-dev/commit-message/config';
+
+/** Build a set of scopes for a package. */
+function buildScopesFor(pkg: string, subpkgs: string[]) {
+  return [pkg, ...subpkgs.map((subpkg: string) => `${pkg}/${subpkg}`)];
+}
+
+export const commitMessage: CommitMessageConfig = {
+  maxLineLength: Infinity,
+  minBodyLength: 0,
+  scopes: [
+    ...buildScopesFor('bazel', ['api-golden', 'benchmark', 'browsers', 'remote-execution']),
+    ...buildScopesFor('github-actions', [
+      'breaking-changes-label',
+      'feature-request',
+      'lock-closed',
+      'rebase',
+      'rerun-circleci',
+    ]),
+    ...buildScopesFor('ng-dev', [
+      'caretaker',
+      'commit-message',
+      'format',
+      'ng-bot',
+      'pr',
+      'pullapprove',
+      'release',
+      'ts-circular-dependencies',
+    ]),
+  ],
+};
--- .ng-dev/config.ts
+++ .ng-dev/config.ts
@@ -1,3 +1,4 @@
 export {format} from './format';
 export {github} from './github';
 export {merge} from './merge';
+export {commitMessage} from './commit-message';
