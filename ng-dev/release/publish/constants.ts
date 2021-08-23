2e2fa475d87b10de5e752693e3776298122a8a37
Joey Perrott <josephperrott@gmail.com>
2021-08-23 08:09:41 -0700

    fix(ng-dev) update to the body character limit for github releases to 125000 (#174)
    PR Close #174

--- ng-dev/release/publish/constants.ts
+++ ng-dev/release/publish/constants.ts
@@ -16,10 +16,7 @@ export const changelogPath = 'CHANGELOG.md';
 export const waitForPullRequestInterval = 10000;
 
 /**
- * Maximum number of characters a Github release entry can use for its body.
- *
- * Note: Github does not specify an official limit for this, but based on local testing,
- * Github limits switch between 25000 and 125000 characters. We use the lowest limit we have
- * seen so far, as otherwise the limit can potentially be wrong and result in errors.
+ * Maximum number of characters a Github release entry can use for its body.  This number was
+ * confirmed by reaching out to Github support to confirm the character limit.
  */
-export const githubReleaseBodyLimit = 25000;
+export const githubReleaseBodyLimit = 125000;
