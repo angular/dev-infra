e5f8e6d92b73043d561816909db77fbcfb50f0fa
Joey Perrott <josephperrott@gmail.com>
2021-09-16 09:56:34 -0700

    fixup! refactor(ng-dev/pr): move fetch-pull-request into common directory

--- ng-dev/pr/merge/pull-request.ts
+++ ng-dev/pr/merge/pull-request.ts
@@ -4,7 +4,9 @@
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
- */ import {parseCommitMessage} from '../../commit-message/parse';
+ */
+
+import {parseCommitMessage} from '../../commit-message/parse';
 import {PullRequestFailure} from './failures';
 import {matchesPattern} from './string-pattern';
 import {PullRequestMergeTask} from './task';
