25b5c13752161b2692f77c385c596bd4e2a4447d
Joey Perrott <josephperrott@gmail.com>
2021-09-24 14:25:22 -0700

    ci: remove claSignedLabel attribute from ng-dev config

--- .ng-dev/pull-request.ts
+++ .ng-dev/pull-request.ts
@@ -3,7 +3,6 @@ import {PullRequestConfig} from '../ng-dev/pr/config';
 /** Configuration for interacting with pull requests in the repo. */
 export const pullRequest: PullRequestConfig = {
   githubApiMerge: false,
-  claSignedLabel: 'cla: yes',
   mergeReadyLabel: 'action: merge',
   caretakerNoteLabel: 'merge note',
   commitMessageFixupLabel: 'needs commit fixup',
