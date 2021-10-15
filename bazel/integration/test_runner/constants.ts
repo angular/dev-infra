67d76fae49524e337f08dbbdcd6cf6b03189706c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-10-15 19:23:43 +0200

    feat(bazel): add support for custom environment variables in integration tests
    Adds support for custom environment variables in integration tests. The
    environment variable dictionary can use location expansion. Also there
    is a special placeholder called `<TMP>` that can be used to acquire a
    temporary directory. This is useful when setting up tools like BAZELISK,
    which requires a `HOME` environment variable as an example.

---
+++ bazel/integration/test_runner/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * Placeholder that can be used in the test environment dictionary to reserve a
+ * temporary directory. A temporary directory can be useful when running tools
+ * like Bazelisk which need a `HOME` directory for example.
+ */
+export const ENVIRONMENT_TMP_PLACEHOLDER = '<TMP>';
