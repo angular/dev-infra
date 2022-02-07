1c1d5c5d28f1c670ecdf9615c4e62a87f48151ee
Joey Perrott <josephperrott@gmail.com>
2022-02-07 15:38:59 -0800

    fix(ng-dev/pr): do not fail assertions for changes for target label when the fixup label is applied (#392)
    As noted in #301, if the commit message fixup label is applied we cannot properly assert
    whether or not the changes are allowed for the target label.  Instead we should skip this
    assertion.
    
    Fixes #301
    
    PR Close #392

--- ng-dev/pr/common/validation/validations.ts
+++ ng-dev/pr/common/validation/validations.ts
@@ -10,7 +10,7 @@ import {Commit} from '../../../commit-message/parse';
 import {TargetLabel, TargetLabelName} from '../targeting/target-label';
 import {breakingChangeLabel, PullRequestConfig} from '../../config';
 import {PullRequestFailure} from './failures';
-import {red, warn} from '../../../utils/console';
+import {debug, red, warn} from '../../../utils/console';
 import {
   getStatusesForPullRequest,
   PullRequestFromGithub,
@@ -28,7 +28,19 @@ export function assertChangesAllowForTargetLabel(
   label: TargetLabel,
   config: PullRequestConfig,
   releaseTrains: ActiveReleaseTrains,
+  labelsOnPullRequest: string[],
 ) {
+  if (
+    !!config.commitMessageFixupLabel &&
+    labelsOnPullRequest.some((name) => matchesPattern(name, config.commitMessageFixupLabel))
+  ) {
+    debug(
+      'Skipping commit message target label validation because the commit message fixup label is ' +
+        'applied.',
+    );
+    return;
+  }
+
   /**
    * List of commit scopes which are exempted from target label content requirements. i.e. no `feat`
    * scopes in patch branches, no breaking changes in minor or patch changes.
