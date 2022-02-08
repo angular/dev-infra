ea436a0b968c5d76c9d72912cacd771e96baeccb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-02-08 17:37:17 +0100

    build: fix firefox mirror script to also deploy firefox arm64 driver binaries

--- bazel/browsers/update-script/firefox.ts
+++ bazel/browsers/update-script/firefox.ts
@@ -16,11 +16,18 @@ const downloadLinuxUrls = {
     'https://github.com/mozilla/geckodriver/releases/download/v{version}/geckodriver-v{version}-linux64.tar.gz',
 };
 
-const downloadMacOsUrls = {
+const downloadMacOsX64Urls = {
   'browser-bin':
     'https://ftp.mozilla.org/pub/firefox/releases/{version}/mac/en-US/Firefox {version}.dmg',
   'driver-bin':
     'https://github.com/mozilla/geckodriver/releases/download/v{version}/geckodriver-v{version}-macos.tar.gz',
+};
+
+const downloadMacOsArm64Urls = {
+  'browser-bin':
+    'https://ftp.mozilla.org/pub/firefox/releases/{version}/mac/en-US/Firefox {version}.dmg',
+  'driver-bin':
+    'https://github.com/mozilla/geckodriver/releases/download/v{version}/geckodriver-v{version}-macos-aarch64.tar.gz',
 };
 
 /** Class providing necessary information for the firefox browser. */
@@ -30,13 +37,30 @@ export class Firefox implements Browser<string> {
   constructor(public revision: string, public driverVersion: string) {}
 
   supports(platform: Platform): boolean {
-    return platform === Platform.LINUX_X64 || platform === Platform.MAC_X64;
+    return (
+      platform === Platform.LINUX_X64 ||
+      platform === Platform.MAC_X64 ||
+      platform === Platform.MAC_ARM64
+    );
   }
 
   getDownloadUrl(platform: Platform, archiveType: ArchiveType): string {
-    const urlSet = platform === Platform.LINUX_X64 ? downloadLinuxUrls : downloadMacOsUrls;
+    const urlSet = this._getUrlSetForPlatform(platform);
     const baseUrl = urlSet[archiveType];
 
     return baseUrl.replace(/\{version}/g, this.revision);
+  }
+
+  private _getUrlSetForPlatform(platform: Platform): Record<ArchiveType, string> {
+    switch (platform) {
+      case Platform.LINUX_X64:
+        return downloadLinuxUrls;
+      case Platform.MAC_X64:
+        return downloadMacOsX64Urls;
+      case Platform.MAC_ARM64:
+        return downloadMacOsArm64Urls;
+      default:
+        throw Error('Unexpected platform without Firefox support.');
+    }
   }
 }
