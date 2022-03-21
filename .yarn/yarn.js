a7553550ff47d952b1c130866d5be9f4c73c8ac8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-03-21 15:41:55 +0100

    build: update bazel setup to v5.3.0

--- .yarn/yarn.js
+++
@@ -1,6 +0,0 @@
-const {fork} = require('child_process');
-
-// Wrapper script to workaround a bug in `rules_nodejs` where `.cjs`
-// vendored files are not detected and launched as binaries (breaking Windows).
-// TODO: Remove once https://github.com/bazelbuild/rules_nodejs/pull/3350 is available.
-fork(__dirname + '/releases/yarn-3.2.0.cjs', process.argv.slice(2));
