25315ed62e743c934235b13c192c0a80820cf5b4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-03-21 16:39:07 +0100

    test: add test for api-golden type resolution and `npm_package`-resolution mechanism
    Adds a test for the NPM package API golden rule (not tested currently), and expands
    the existing standalone file test to also ensure the `types` option/resolution works.

---
+++ bazel/api-golden/test/fixtures/test_fixture.ts
@@ -0,0 +1,8 @@
+import {rawStringFn, testValue} from './transitive_target';
+
+export {testValue, rawStringFn};
+
+export const NodeTypes: NodeJS.ProcessEnv = {};
+
+export class SomeOtherExport {}
+export class ɵShouldBeIgnored {}
---
+++ bazel/api-golden/test/fixtures/transitive_target/index.ts
@@ -0,0 +1,5 @@
+export const testValue = `hello`;
+
+export function rawStringFn(input: string) {
+  return input;
+}
