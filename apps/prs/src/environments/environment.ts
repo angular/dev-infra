11e58e35a6dd1d15e709fe2dd5fa1b24cb34c437
Joey Perrott <josephperrott@gmail.com>
2022-04-04 12:24:16 -0700

    feat(apps): create base of prs application
    Establish the base angular application and theming for prs application.

---
+++ apps/prs/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyDM3rXWUgYuxYCmBKwnZvvnraYoYIE5_5U',
+    authDomain: 'internal-200822.firebaseapp.com',
+    projectId: 'internal-200822',
+    storageBucket: 'internal-200822.appspot.com',
+    messagingSenderId: '823469418460',
+    appId: '1:823469418460:web:009b51c93132b218761119',
+  },
+  production: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
