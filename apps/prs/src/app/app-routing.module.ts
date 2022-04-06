50857c6aa6532660e84d2974272f6b0041e291b8
Joey Perrott <josephperrott@gmail.com>
2022-04-06 16:10:13 -0700

    feat(apps): create login page (#502)
    Create the login page for the prs application.
    
    PR Close #502

--- apps/prs/src/app/app-routing.module.ts
+++ apps/prs/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
