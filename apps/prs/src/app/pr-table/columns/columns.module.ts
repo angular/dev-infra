5d04dfc47eb51fa2d3ea857a3568117a866df3f1
Joey Perrott <josephperrott@gmail.com>
2022-04-19 15:00:00 -0700

    feat(apps): create target column
    Create a column which displays the current target of the pull request

--- apps/prs/src/app/pr-table/columns/columns.module.ts
+++ apps/prs/src/app/pr-table/columns/columns.module.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 
 import {MatTableModule} from '@angular/material/table';
 import {SummaryColumn} from './summary/summary.component';
+import {TargetColumn} from './target/target.component';
 import {StatusColumn} from './status/status.component';
 import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  declarations: [SummaryColumn, StatusColumn],
+  declarations: [SummaryColumn, TargetColumn, StatusColumn],
   imports: [CommonModule, MatTableModule, MatIconModule],
 })
 export class ColumnsModule {}
--- apps/prs/src/models/pull-request.ts
+++ apps/prs/src/models/pull-request.ts
@@ -1,3 +1,11 @@
 import {PullRequest as SharedPullRequest} from '../../../shared/models/app-models';
+import {FirestorePullRequest} from '../../../shared/models/pull-request';
 
-export class PullRequest extends SharedPullRequest {}
+export class PullRequest extends SharedPullRequest {
+  target: string | undefined;
+
+  override async setData(data: FirestorePullRequest) {
+    await super.setData(data);
+    this.target = this.labels.find((l) => l?.name?.startsWith('target:'))?.name || '';
+  }
+}
