eea28e123a6e303326bf4af9b2e5a877885af929
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-06-01 17:34:32 +0000

    refactor(ng-dev): introduce utility for reading buffer from stdin
    Introduces an utility for reading buffer/text from process stdin. This
    will be useful for delivering larger data from the release tool to
    external commands. Command line arguments are not sufficient-enough
    sine they suffer from escaping problems and max-length limits.

---
+++ ng-dev/utils/read-stdin-until-closed.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/** Unique error class for failures when reading from the stdin. */
+export class ReadBufferFromStdinError extends Error {}
+
+/**
+ * Reads a `Buffer` from `stdin` until the stream is closed.
+ *
+ * @returns a Promise resolving with the `Buffer`. Rejects with `ReadBufferFromStdinError`
+ *   on unexpected read errors.
+ */
+export function readBufferFromStdinUntilClosed(
+  input: NodeJS.ReadStream = process.stdin,
+): Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    const data: Buffer[] = [];
+
+    input.on('data', (chunk) => data.push(chunk));
+    input.on('end', () => resolve(Buffer.concat(data)));
+    input.on('error', () => reject(new ReadBufferFromStdinError()));
+    input.on('timeout', () => reject(new ReadBufferFromStdinError('Unexpected timeout')));
+  });
+}
