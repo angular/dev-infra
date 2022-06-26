a628d8d6f45c10b5a5d5e1db1b555f468e8b11f6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-06-26 10:13:25 +0000

    ci: setup self-hosted renovate for angular

---
+++ .github/ng-renovate/README.md
@@ -0,0 +1,4 @@
+### Workspace for the Angular renovate workflow.
+
+The dependency for Renovate is rather large and is managed separately from
+the main workspace to avoid impacting DX.
---
+++ .github/ng-renovate/runner-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  branchPrefix: 'ng-renovate/',
+  gitAuthor: 'Angular Robot <angulat-robot@google.com>',
+  platform: 'github',
+  forkMode: true,
+  onboarding: false,
+  repositories: ['angular/dev-infra', 'angular/components'],
+  productLinks: {
+    documentation: 'https://docs.renovatebot.com/',
+    help: 'https://github.com/angular/dev-infra',
+    homepage: 'https://github.com/angular/dev-infra',
+  },
+};
