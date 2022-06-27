00f441f423a750c949505697bc5b2bdbdb979efe
Joey Perrott <josephperrott@gmail.com>
2022-06-27 19:26:27 +0000

    feat(ng-dev): validate all licenses as being allowed for our Angular projects
    A tool for validating that all packages provide allowed licenses in our projects.

--- ng-dev/misc/cli.ts
+++ ng-dev/misc/cli.ts
@@ -10,6 +10,7 @@ import {Argv} from 'yargs';
 import {BuildAndLinkCommandModule} from './build-and-link/cli.js';
 import {NewMainBranchCommandModule} from './new-main-branch/cli.js';
 import {UpdateYarnCommandModule} from './update-yarn/cli.js';
+import { ValidateLicensesModule } from './validate-licenses/cli.js';
 
 /** Build the parser for the misc commands. */
 export function buildMiscParser(localYargs: Argv) {
@@ -18,5 +19,6 @@ export function buildMiscParser(localYargs: Argv) {
     .strict()
     .command(BuildAndLinkCommandModule)
     .command(NewMainBranchCommandModule)
-    .command(UpdateYarnCommandModule);
+    .command(UpdateYarnCommandModule)
+    .command(ValidateLicensesModule);
 }
---
+++ ng-dev/misc/validate-licenses/cli.ts
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import { Argv, Arguments, CommandModule } from 'yargs';
+import { Log, green, red } from '../../utils/logging.js';
+import { determineRepoBaseDirFromCwd } from '../../utils/repo-directory.js';
+
+import { checkAllLicenses } from './validate.js';
+
+/** Command line options. */
+export interface Options {}
+
+/** Yargs command builder for the command. */
+function builder(argv: Argv): Argv<Options> {
+  return argv;
+}
+
+/** Yargs command handler for the command. */
+async function handler({ }: Arguments<Options>) {
+  await checkAllLicenses(determineRepoBaseDirFromCwd()).then(({ valid, maxPkgNameLength, packages }) => {
+    if (valid) {
+      Log.info(`  ${green('✓')}  All discovered licenses comply with our restrictions (${packages.length} packages)`);
+      return;
+    }
+
+    Log.info(red(' ✘ The following packages were found to have disallowed licenses:\n'))
+    Log.info(`${'     Package Name'.padEnd(maxPkgNameLength)}     |      LICENSE`);
+    packages.filter(pkg => !pkg.allowed).forEach(pkg => {
+      Log.info(`  - ${pkg.name.padEnd(maxPkgNameLength)} | ${pkg.licenses}`);
+    });
+    process.exitCode = 1;
+  },
+    (reason: Error) => {
+      Log.info(red(' ✘ An error occured while processing package licenses:'))
+      Log.error(reason);
+      process.exitCode = 1;
+    });
+}
+
+/** CLI command module. */
+export const ValidateLicensesModule: CommandModule<{}, Options> = {
+  builder,
+  handler,
+  command: 'validate-licenses',
+  describe: 'Validate the licenses for all dependencies for the repo',
+};
