8949183a6608081ecf75a8207d130d86158edb47
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-06-28 19:36:33 +0000

    feat(bazel): move js mapping size tracking tool to dev-infra
    Moves the JS mapping size tracking tool from framework into the
    dev-infra repo. The following additonal changes have been made:
    
    * Reworked the API to not require manifest paths, but to support
      validatable Bazel labels as rule parameters.
    * Adjustments to make it shippable from `bazel/`
    * Additional testing to verify the Starlark code
    * Update to account for the latest source-map types. i.e. making
      size-tracking asynchronous.

---
+++ bazel/map-size-tracking/test/fixture-app/main.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Observable} from 'rxjs';
+import {TestClass} from './test-component.js';
+
+console.log(TestClass, Observable);
