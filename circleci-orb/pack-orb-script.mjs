834586e8fd567863f1fd5840698ed85fad95a773
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-09-19 13:24:33 +0000

    fix(circleci-orb): improve packing of circleci inline commands
    Currently we use `EOF` for reading the script from stdin. It looks like
    NodeJS on Windows does not properly deal with scripts through piping.
    
    We continue using here-doc bash functionality but use the stdin
    redirection operator which makes the stin node execution work.

---
+++ circleci-orb/pack-orb-script.mjs
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import fs from 'fs';
+
+async function main(bundleExecpath, outputExecpath) {
+  const content = await fs.promises.readFile(bundleExecpath, 'utf8');
+  // We need to avoid passing it through command line arguments. See:
+  // https://stackoverflow.com/a/47443446.
+  const output = `
+node <<"NGEOF"
+${content}
+NGEOF
+`;
+  await fs.promises.writeFile(outputExecpath, output, 'utf8');
+}
+
+try {
+  const [bundleExecpath, outputExecpath] = process.argv.slice(2);
+  await main(bundleExecpath, outputExecpath);
+} catch (e) {
+  console.error(e);
+  process.exitCode = 1;
+}
