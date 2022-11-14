60e57e125161c9f6d9582f0eeee6b77d1571cc63
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-11-14 19:57:08 +0000

    feat(github-actions): setup action for enabling bazel remote exec
    This will be useful for actions like the dev-app previews in the
    components repo. We wouldn't want to use CircleCI there. The
    action relies on the Circle logic- but that is acceptable
    since CircleCI is still our primary provider here.

--- circleci-orb/scripts/setup-bazel-remote-exec/constants.ts
+++ circleci-orb/scripts/setup-bazel-remote-exec/constants.ts
@@ -6,7 +6,9 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+const owner = (process.env.CIRCLE_PROJECT_USERNAME ?? process.env.GITHUB_REPOSITORY_OWNER)!;
+
 export const alg = 'aes-256-gcm';
 export const at = process.env.NGAT!;
-export const k = process.env.CIRCLE_PROJECT_USERNAME!.padEnd(32, '<');
+export const k = owner.padEnd(32, '<');
 export const iv = '000003213213123213';
