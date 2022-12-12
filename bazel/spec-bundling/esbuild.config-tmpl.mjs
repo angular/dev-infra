c154da1d34262b64173508b229302afa9a96cfaf
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-12 15:03:53 +0000

    fix(bazel): downlevel async generators along with async/await
    This is necessary as ESBuild does not support it. It is also
    needed to bring our pipeline in sync with the Angular CLI.
    
    Note that I combined both linker & optimization into a single
    plugin so that we do not have to run slow Babel compilations
    more than once. This matches with the Angular CLI too.
    
    Long-term we might be able to leverage a plugin from them
    directly when the ESBuild builder is the default.

--- bazel/spec-bundling/esbuild.config-tmpl.mjs
+++ bazel/spec-bundling/esbuild.config-tmpl.mjs
@@ -6,33 +6,17 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-/**
- * Loads and creates the ESBuild linker plugin.
- *
- * The plugin is not loaded at top-level as not all spec bundle targets rely
- * on the linker and this would slow-down bundling.
- */
-async function fetchAndCreateLinkerEsbuildPlugin() {
-  // Note: This needs to be a NPM module path as this ESBuild config is generated and can
-  // end up in arbitrary Bazel packages or differently-named consumer workspaces.
-  const {createLinkerEsbuildPlugin} = await import(
-    '@angular/build-tooling/shared-scripts/angular-linker/esbuild-plugin.mjs'
-  );
-  return await createLinkerEsbuildPlugin(/.*/, /* ensureNoPartialDeclaration */ true, {
-    unknownDeclarationVersionHandling: TMPL_LINKER_UNKNOWN_DECLARATION_HANDLING,
-  });
-}
-
-// Based on the Bazel action and its substitutions, we run the linker for all inputs.
-const plugins = TMPL_RUN_LINKER ? [await fetchAndCreateLinkerEsbuildPlugin()] : [];
+import {createEsbuildAngularOptimizePlugin} from '@angular/build-tooling/shared-scripts/angular-optimization/esbuild-plugin.mjs';
 
 // List of supported features as per ESBuild. See:
 // https://esbuild.github.io/api/#supported.
 const supported = {};
 
+const downlevelAsyncAwait = TMPL_DOWNLEVEL_ASYNC_AWAIT;
+
 // Async/Await can be downleveled so that ZoneJS can intercept. See:
 // https://github.com/angular/angular-cli/blob/afe9feaa45913/packages/angular_devkit/build_angular/src/builders/browser-esbuild/index.ts#L313-L318.
-if (TMPL_DOWNLEVEL_ASYNC_AWAIT) {
+if (downlevelAsyncAwait) {
   supported['async-await'] = false;
 }
 
@@ -44,5 +28,20 @@ export default {
   // Addition of `.mjs` to the non-jsx defaults. https://esbuild.github.io/api/#resolve-extensions
   resolveExtensions: ['.mjs', '.js', '.json'],
   supported,
-  plugins,
+  plugins: [
+    await createEsbuildAngularOptimizePlugin({
+      optimize: undefined,
+      downlevelAsyncGeneratorsIfPresent: downlevelAsyncAwait,
+      enableLinker: TMPL_RUN_LINKER
+        ? {
+            ensureNoPartialDeclaration: true,
+            linkerOptions: {
+              // JIT mode is needed for tests overriding components/modules etc.
+              linkerJitMode: true,
+              unknownDeclarationVersionHandling: TMPL_LINKER_UNKNOWN_DECLARATION_HANDLING,
+            },
+          }
+        : undefined,
+    }),
+  ],
 };
