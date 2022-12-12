26d30c4e37cf9f0f8ae62b1c8fe4e03e5474ab38
Joey Perrott <josephperrott@gmail.com>
2022-12-12 15:35:12 +0000

    refactor(github-actions): use class for pull request instead of object

--- github-actions/unified-status-check/src/main.ts
+++ github-actions/unified-status-check/src/main.ts
@@ -1,8 +1,7 @@
 import * as core from '@actions/core';
-import {context} from '@actions/github';
 import {Octokit} from '@octokit/rest';
 import {getAuthTokenFor, ANGULAR_ROBOT, revokeActiveInstallationToken} from '../../utils.js';
-import {getPullRequest, NormalizedState, unifiedStatusCheckName} from './pull-request.js';
+import {PullRequest} from './pull-request.js';
 import {isDraft} from './draft-mode.js';
 import {checkOnlyPassingStatuses, checkRequiredStatuses} from './statuses.js';
 import {checkForTargelLabel, isMergeReady} from './labels.js';
@@ -13,59 +12,45 @@ async function main() {
 
   try {
     /** The pull request triggering the event */
-    const pullRequest = await getPullRequest(github);
-    const unifiedCheckStatus = pullRequest.statuses.unifiedCheckStatus;
-
-    const setStatus = async (state: NormalizedState, description?: string) => {
-      if (
-        unifiedCheckStatus &&
-        unifiedCheckStatus.state === state &&
-        unifiedCheckStatus.description === description
-      ) {
-        console.log(
-          'Skipping status update as the request status and information is the same as the current status',
-        );
-        return;
-      }
-
-      await github.repos.createCommitStatus({
-        ...context.repo,
-        sha: pullRequest.sha,
-        context: unifiedStatusCheckName,
-        state,
-        description,
-      });
-      return;
-    };
+    const pullRequest = await PullRequest.get(github);
 
     /** If no status checks are present, or if the pull request is in a draft state the unified status is in a pending state. */
     const isDraftValidationResult = isDraft(pullRequest);
     if (isDraftValidationResult.state === 'pending') {
-      await setStatus(isDraftValidationResult.state, isDraftValidationResult.description);
+      await pullRequest.setCheckResult(
+        isDraftValidationResult.state,
+        isDraftValidationResult.description,
+      );
       return;
     }
 
     const isMergeReadyResult = isMergeReady(pullRequest);
     if (isMergeReadyResult.state === 'pending') {
-      await setStatus(isMergeReadyResult.state, isMergeReadyResult.description);
+      await pullRequest.setCheckResult(isMergeReadyResult.state, isMergeReadyResult.description);
       return;
     }
 
     const requiredStatusesResult = checkRequiredStatuses(pullRequest);
     if (requiredStatusesResult.state === 'pending') {
-      await setStatus(requiredStatusesResult.state, requiredStatusesResult.description);
+      await pullRequest.setCheckResult(
+        requiredStatusesResult.state,
+        requiredStatusesResult.description,
+      );
       return;
     }
 
     const onlyPassingStatusesResult = checkOnlyPassingStatuses(pullRequest);
     if (onlyPassingStatusesResult.state === 'pending') {
-      await setStatus(onlyPassingStatusesResult.state, onlyPassingStatusesResult.description);
+      await pullRequest.setCheckResult(
+        onlyPassingStatusesResult.state,
+        onlyPassingStatusesResult.description,
+      );
       return;
     }
 
     const targetLabelResult = checkForTargelLabel(pullRequest);
     if (targetLabelResult.state === 'pending') {
-      await setStatus(targetLabelResult.state, targetLabelResult.description);
+      await pullRequest.setCheckResult(targetLabelResult.state, targetLabelResult.description);
       return;
     }
   } finally {
