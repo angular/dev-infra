ccc9ee1059cd2dcddfbe7e4ee9c3eca497ec7d41
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-12 13:57:11 +0000

    feat(bazel): support using es2020 in spec-bundled tests
    We currently set the default spec-bundle target to es2016 to work
    around the ZoneJS async/await native syntax issue. We can default
    to ES2020 consistently and just downlevel async/await, similar to
    how the Angular CLI would do it.

--- bazel/spec-bundling/esbuild.config-tmpl.mjs
+++ bazel/spec-bundling/esbuild.config-tmpl.mjs
@@ -26,6 +26,16 @@ async function fetchAndCreateLinkerEsbuildPlugin() {
 // Based on the Bazel action and its substitutions, we run the linker for all inputs.
 const plugins = TMPL_RUN_LINKER ? [await fetchAndCreateLinkerEsbuildPlugin()] : [];
 
+// List of supported features as per ESBuild. See:
+// https://esbuild.github.io/api/#supported.
+const supported = {};
+
+// Async/Await can be downleveled so that ZoneJS can intercept. See:
+// https://github.com/angular/angular-cli/blob/afe9feaa45913/packages/angular_devkit/build_angular/src/builders/browser-esbuild/index.ts#L313-L318.
+if (TMPL_DOWNLEVEL_ASYNC_AWAIT) {
+  supported['async-await'] = false;
+}
+
 export default {
   // `tslib` sets the `module` condition to resolve to ESM.
   conditions: ['es2020', 'es2015', 'module'],
@@ -33,5 +43,6 @@ export default {
   mainFields: ['es2020', 'es2015', 'module', 'main'],
   // Addition of `.mjs` to the non-jsx defaults. https://esbuild.github.io/api/#resolve-extensions
   resolveExtensions: ['.mjs', '.js', '.json'],
+  supported,
   plugins,
 };
