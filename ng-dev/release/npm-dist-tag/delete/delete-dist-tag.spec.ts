9cf75915e1a040d4fbf651076df6823568b956ed
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-03 14:54:45 +0000

    feat(ng-dev): introduce combined command for dealing with NPM dist tags
    Instead of having a commmand like `release set-dist-tag` we start
    having a combined command for dealing with NPM dist tags.
    
    e.g. `release npm-dist-tag <set|delete>`.
    
    This commit introduces this structure, moves the existing set dist tag
    command over, and adds a command for deleting.

---
+++ ng-dev/release/npm-dist-tag/delete/delete-dist-tag.spec.ts
@@ -0,0 +1,100 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {NpmCommand} from '../../versioning/npm-command.js';
+import {NpmPackage} from '../../config/index.js';
+
+import {ReleaseNpmDistTagDeleteCommand} from './cli.js';
+import {ReleaseConfig} from '../../config/index.js';
+import {setConfig} from '../../../utils/config.js';
+
+describe('ng-dev release npm-dist-tag delete', () => {
+  let npmPackages: NpmPackage[];
+  let publishRegistry: string | undefined;
+
+  beforeEach(() => {
+    npmPackages = [{name: '@angular/pkg1'}, {name: '@angular/pkg2'}];
+    publishRegistry = undefined;
+
+    spyOn(NpmCommand, 'deleteDistTagForPackage');
+    // We need to stub out the `process.exit` function during tests as the
+    // CLI handler calls those in case of failures.
+    spyOn(process, 'exit');
+  });
+
+  /** Invokes the command handler. */
+  async function invokeCommand(tagName: string) {
+    setConfig({
+      release: {
+        representativeNpmPackage: npmPackages[0].name,
+        npmPackages,
+        publishRegistry,
+        buildPackages: async () => [],
+      } as ReleaseConfig,
+    });
+    await ReleaseNpmDistTagDeleteCommand.handler({
+      tagName,
+      $0: '',
+      _: [],
+    });
+  }
+
+  it('should invoke "npm dist-tag" command for all configured packages', async () => {
+    await invokeCommand('some-tag');
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledTimes(2);
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledWith(
+      '@angular/pkg1',
+      'some-tag',
+      undefined,
+    );
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledWith(
+      '@angular/pkg2',
+      'some-tag',
+      undefined,
+    );
+  });
+
+  it('should always invoke "npm dist-tag" for experimental packages', async () => {
+    npmPackages.push({name: '@whatever/experimental', experimental: true});
+    await invokeCommand('some-tag');
+
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledTimes(3);
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledWith(
+      '@angular/pkg1',
+      'some-tag',
+      undefined,
+    );
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledWith(
+      '@angular/pkg2',
+      'some-tag',
+      undefined,
+    );
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledWith(
+      '@whatever/experimental',
+      'some-tag',
+      undefined,
+    );
+  });
+
+  it('should support a configured custom NPM registry', async () => {
+    publishRegistry = 'https://my-custom-registry.angular.io';
+    await invokeCommand('some-tag');
+
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledTimes(2);
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledWith(
+      '@angular/pkg1',
+      'some-tag',
+      'https://my-custom-registry.angular.io',
+    );
+    expect(NpmCommand.deleteDistTagForPackage).toHaveBeenCalledWith(
+      '@angular/pkg2',
+      'some-tag',
+      'https://my-custom-registry.angular.io',
+    );
+  });
+});
---
+++ ng-dev/release/npm-dist-tag/set/cli.ts
@@ -0,0 +1,111 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+// ---- **IMPORTANT** ----
+// This command is part of our external commands invoked by the release publish
+// command. Before making changes, keep in mind that more recent `ng-dev` versions
+// can still invoke this command.
+// ------------------------
+
+import semver from 'semver';
+import {Argv, Arguments, CommandModule} from 'yargs';
+import {getConfig} from '../../../utils/config.js';
+
+import {Log, bold, green} from '../../../utils/logging.js';
+import {Spinner} from '../../../utils/spinner.js';
+import {assertValidReleaseConfig} from '../../config/index.js';
+import {NpmCommand} from '../../versioning/npm-command.js';
+import {
+  createExperimentalSemver,
+  isExperimentalSemver,
+} from '../../versioning/experimental-versions.js';
+
+/** Command line options for setting an NPM dist tag. */
+export interface ReleaseNpmDistTagSetOptions {
+  tagName: string;
+  targetVersion: string;
+  skipExperimentalPackages: boolean;
+}
+
+function builder(args: Argv): Argv<ReleaseNpmDistTagSetOptions> {
+  return args
+    .positional('tagName', {
+      type: 'string',
+      demandOption: true,
+      description: 'Name of the NPM dist tag.',
+    })
+    .positional('targetVersion', {
+      type: 'string',
+      demandOption: true,
+      description:
+        'Version to which the NPM dist tag should be set.\nThis version will be ' +
+        'converted to an experimental version for experimental packages.',
+    })
+    .option('skipExperimentalPackages', {
+      type: 'boolean',
+      description: 'Whether the dist tag should not be set for experimental NPM packages.',
+      default: false,
+    });
+}
+
+/** Yargs command handler for setting an NPM dist tag. */
+async function handler(args: Arguments<ReleaseNpmDistTagSetOptions>) {
+  const {targetVersion: rawVersion, tagName, skipExperimentalPackages} = args;
+  const config = await getConfig();
+  assertValidReleaseConfig(config);
+  const {npmPackages, publishRegistry} = config.release;
+  const version = semver.parse(rawVersion);
+
+  if (version === null) {
+    Log.error(`Invalid version specified (${rawVersion}). Unable to set NPM dist tag.`);
+    process.exit(1);
+  } else if (isExperimentalSemver(version)) {
+    Log.error(
+      `Unexpected experimental SemVer version specified. This command expects a ` +
+        `non-experimental project SemVer version.`,
+    );
+    process.exit(1);
+  }
+
+  Log.debug(`Setting "${tagName}" NPM dist tag for release packages to v${version}.`);
+  const spinner = new Spinner('');
+
+  for (const pkg of npmPackages) {
+    // If `--skip-experimental-packages` is specified, all NPM packages which
+    // are marked as experimental will not receive the NPM dist tag update.
+    if (pkg.experimental && skipExperimentalPackages) {
+      spinner.update(`Skipping "${pkg.name}" due to it being experimental.`);
+      continue;
+    }
+
+    spinner.update(`Setting NPM dist tag for "${pkg.name}"`);
+    const distTagVersion = pkg.experimental ? createExperimentalSemver(version!) : version!;
+
+    try {
+      await NpmCommand.setDistTagForPackage(pkg.name, tagName, distTagVersion, publishRegistry);
+      Log.debug(`Successfully set "${tagName}" NPM dist tag for "${pkg.name}".`);
+    } catch (e) {
+      spinner.complete();
+      Log.error(e);
+      Log.error(`  ✘   An error occurred while setting the NPM dist tag for "${pkg.name}".`);
+      process.exit(1);
+    }
+  }
+
+  spinner.complete();
+  Log.info(green(`  ✓   Set NPM dist tag for all release packages.`));
+  Log.info(green(`      ${bold(tagName)} will now point to ${bold(`v${version}`)}.`));
+}
+
+/** CLI command module for setting an NPM dist tag. */
+export const ReleaseNpmDistTagSetCommand: CommandModule<{}, ReleaseNpmDistTagSetOptions> = {
+  builder,
+  handler,
+  command: 'set <tag-name> <target-version>',
+  describe: 'Sets a given NPM dist tag for all release packages.',
+};
