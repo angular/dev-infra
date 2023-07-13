c50ea8894a5d6f048f571016dd09e8aa02f0d542
Joey Perrott <josephperrott@gmail.com>
2023-07-13 16:47:19 +0000

    feat(github-actions): set the config for remote-cache on windows CI runs (#1271)
    Set the remote-cache configuration when setting up bazel remote for windows CI runs
    
    PR Close #1271

--- circleci-orb/scripts/setup-bazel-remote-exec/index.ts
+++ circleci-orb/scripts/setup-bazel-remote-exec/index.ts
@@ -15,21 +15,26 @@ import fs from 'fs';
 import os from 'os';
 
 async function main(bazelRcPath: string | undefined) {
+  const isWindows = os.platform() === 'win32';
   const t: Uint8Array = tokenRaw;
   const dcip = createDecipheriv(alg, k, iv).setAuthTag(Buffer.from(at, 'base64'));
   const dec = dcip.update(t, undefined, 'utf8') + dcip.final('utf8');
 
-  const destPath =
-    os.platform() === 'win32'
-      ? path.join(process.env.APPDATA!, 'gcloud/application_default_credentials.json')
-      : path.join(process.env.HOME!, '.config/gcloud/application_default_credentials.json');
+  const destPath = isWindows
+    ? path.join(process.env.APPDATA!, 'gcloud/application_default_credentials.json')
+    : path.join(process.env.HOME!, '.config/gcloud/application_default_credentials.json');
 
   await fs.promises.mkdir(path.dirname(destPath), {recursive: true});
   await fs.promises.writeFile(destPath, dec, 'utf8');
 
   if (bazelRcPath) {
     let content = await readFileGracefully(bazelRcPath);
-    content += '\nbuild --config=remote';
+    if (isWindows) {
+      // Set the config to remote-cache as we do not have support for RBE on windows at this time
+      content += '\nbuild --config=remote-cache';
+    } else {
+      content += '\nbuild --config=remote';
+    }
     await fs.promises.writeFile(bazelRcPath, content, 'utf8');
   }
 }
