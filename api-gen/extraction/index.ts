264811d5dad9272ab8a4ec189f1509151e9a0752
Jeremy Elbourn <jelbourn@google.com>
2023-09-14 20:58:57 -0700

    feat(api-gen): set up Ngtsc program for raw api extraction (#1393)
    Based on top of #1392
    
    This adds the setup for creating an Ngtsc program to pull API data from files.
    
    PR Close #1393

--- api-gen/extraction/index.ts
+++ api-gen/extraction/index.ts
@@ -1,11 +1,21 @@
 import {writeFileSync} from 'fs';
+// @ts-ignore This compiles fine, but Webstorm doesn't like the ESM import in a CJS context.
+import {NgtscProgram, CompilerOptions, createCompilerHost} from '@angular/compiler-cli';
 
 function main() {
-  const [srcs, outputFileName] = process.argv.slice(2);
+  const [srcs, outputFilenameExecRootRelativePath] = process.argv.slice(2);
 
-  // TODO: read data with APIs from @angular/compiler-cli
+  const compilerOptions: CompilerOptions = {};
+  const compilerHost = createCompilerHost({options: compilerOptions});
+  const program: NgtscProgram = new NgtscProgram(srcs.split(','), compilerOptions, compilerHost);
+  console.log(`NgtscProgram successfully created: ${!!program}`);
 
-  writeFileSync(outputFileName, JSON.stringify({entries: []}), {encoding: 'utf8'});
+  // TODO: call the real API when it is published to npm.
+  //     When tested with a locally built @angular/compiler-cli, it works!
+  // const output = JSON.stringify(program.getApiDocumentation());
+  const output = JSON.stringify({entries: []});
+
+  writeFileSync(outputFilenameExecRootRelativePath, output, {encoding: 'utf8'});
 }
 
 main();
