e4cb4fcb02ac84ef1427331f6dbcccf5a56d9e58
Jeremy Elbourn <jelbourn@google.com>
2023-10-02 17:06:21 -0700

    feat(api-gen): re-add entry_point attr (#1426)
    This change was originally made in 62e28b76575609d5fdf5fa4028126d3897a0d60f, but was accidentally lost in 04cdd7b188a6358a9012ea2d6685ef98d7494eb8 (likely due to a stale local branch).
    
    PR Close #1426

--- bazel/api-gen/extraction/index.ts
+++ bazel/api-gen/extraction/index.ts
@@ -3,17 +3,17 @@ import {writeFileSync} from 'fs';
 import {NgtscProgram, CompilerOptions, createCompilerHost} from '@angular/compiler-cli';
 
 function main() {
-  const [moduleName, srcs, outputFilenameExecRootRelativePath] = process.argv.slice(2);
+  const [moduleName, entryPointExecRootRelativePath, srcs, outputFilenameExecRootRelativePath] =
+    process.argv.slice(2);
 
   const compilerOptions: CompilerOptions = {};
   const compilerHost = createCompilerHost({options: compilerOptions});
   const program: NgtscProgram = new NgtscProgram(srcs.split(','), compilerOptions, compilerHost);
-  console.log(`NgtscProgram successfully created: ${!!program}`);
 
-  // TODO: call the real API when it is published to npm.
-  //     When tested with a locally built @angular/compiler-cli, it works!
-  // const output = JSON.stringify(program.getApiDocumentation());
-  const output = JSON.stringify({moduleName: moduleName, entries: []});
+  const output = JSON.stringify({
+    moduleName: moduleName,
+    entries: program.getApiDocumentation(entryPointExecRootRelativePath),
+  });
 
   writeFileSync(outputFilenameExecRootRelativePath, output, {encoding: 'utf8'});
 }
