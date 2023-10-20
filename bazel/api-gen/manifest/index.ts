543ae4ca375c440f39a956e745d54558f5d04d1a
Jeremy Elbourn <jelbourn@google.com>
2023-10-19 17:18:37 -0700

    feat(api-gen): add deprecated flag to manifest, filter duplicates
    Some symbol names appear more than once in the case of function overloads; this commit filters these duplicate entries and additionally adds a deprecated flag to the results.

--- bazel/api-gen/manifest/index.ts
+++ bazel/api-gen/manifest/index.ts
@@ -1,18 +1,5 @@
 import {readFileSync, writeFileSync} from 'fs';
-
-/** The JSON data file format for extracted API reference info. */
-interface EntryCollection {
-  moduleName: string;
-  entries: {name: string; entryType: string}[];
-}
-
-export interface ManifestEntry {
-  name: string;
-  type: string;
-}
-
-/** Manifest that maps each module name to a list of API symbols. */
-type Manifest = Record<string, ManifestEntry[]>;
+import {EntryCollection, generateManifest} from './generate_manifest';
 
 function main() {
   const [srcs, outputFilenameExecRootRelativePath] = process.argv.slice(2);
@@ -22,13 +9,7 @@ function main() {
     .map((srcPath) => readFileSync(srcPath, {encoding: 'utf8'}));
   const apiCollections = sourceContents.map((s) => JSON.parse(s) as EntryCollection);
 
-  const manifest: Manifest = apiCollections.reduce((result, collection) => {
-    return {
-      ...result,
-      [collection.moduleName]: collection.entries.map((e) => ({name: e.name, type: e.entryType})),
-    };
-  }, {});
-
+  const manifest = generateManifest(apiCollections);
   writeFileSync(outputFilenameExecRootRelativePath, JSON.stringify(manifest), {encoding: 'utf8'});
 }
 
