43f424f003987829b6c1375478936d155fd47219
Joey Perrott <josephperrott@gmail.com>
2023-11-13 23:20:49 +0000

    refactor(bazel/markdown-to-html): refactor markdown_to_html macro to use pipeline (#1523)
    Use the pipeline created for markdown_to_html macro
    
    PR Close #1523

--- bazel/markdown_to_html/index.ts
+++ bazel/markdown_to_html/index.ts
@@ -1,17 +1,21 @@
 import {readFileSync, writeFileSync} from 'fs';
-import {markdownFilenameToHtmlFilename, markdownToHtml} from './markdown_to_html';
 import path from 'path';
+import {parseMarkdown} from './src';
 
-function main() {
+async function main() {
   const [paramFilePath] = process.argv.slice(2);
   const rawParamLines = readFileSync(paramFilePath, {encoding: 'utf8'}).split('\n');
   const [srcs, outputFilenameExecRootRelativePath] = rawParamLines;
 
-  for (const markdownSourcePath of srcs.split(',')) {
-    const markdownContent = readFileSync(markdownSourcePath, {encoding: 'utf8'});
-    const htmlOutputContent = markdownToHtml(markdownContent);
+  for (const filePath of srcs.split(',')) {
+    if (!filePath.endsWith('.md')) {
+      throw new Error(`Input file "${filePath}" does not end in a ".md" file extension.`);
+    }
 
-    const htmlFileName = markdownFilenameToHtmlFilename(markdownSourcePath);
+    const markdownContent = readFileSync(filePath, {encoding: 'utf8'});
+    const htmlOutputContent = await parseMarkdown(markdownContent);
+
+    const htmlFileName = filePath.substring(0, filePath.length - '.md'.length) + '.html';
     const htmlOutputPath = path.join(outputFilenameExecRootRelativePath, htmlFileName);
 
     writeFileSync(htmlOutputPath, htmlOutputContent, {encoding: 'utf8'});
