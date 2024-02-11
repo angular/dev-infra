cacb4dddf008a0927298ce1feb7829360c7f26c8
ahmadhakeem18 <ahmadhakeem1894@outlook.com>
2024-02-11 13:47:11 -0800

    fix(shared-docs): include query string in examples' copied link to enable right navigation to the current active tab

--- docs/components/example-viewer/example-viewer.component.ts
+++ docs/components/example-viewer/example-viewer.component.ts
@@ -127,7 +127,7 @@ export class ExampleViewer {
 
   copyLink(): void {
     // Reconstruct the URL using `origin + pathname` so we drop any pre-existing hash.
-    const fullUrl = location.origin + location.pathname + '#example-' + this.exampleMetadata()?.id;
+    const fullUrl = location.origin + location.pathname + location.search + '#example-' + this.exampleMetadata()?.id;
     this.clipboard.copy(fullUrl);
   }
 
