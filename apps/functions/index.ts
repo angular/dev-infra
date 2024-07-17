963ace375ea7c38c76c91b4039e0b4cf728ea061
Joey Perrott <josephperrott@gmail.com>
2024-07-17 14:04:56 +0000

    feat(apps): create an application and associated function for perform DNS redirects to URLs (#2175)
    Create a firebase app which executes a cloud function that redirects known subdomains to specific URLs. This is necessary as a
    replacement to the feature being available from Google Domains as we have moved away from Google Domains. This initial setup properly
    redirects update.angular.{io,dev} and cli.angular.{io,dev} to the correct locations
    
    PR Close #2175

--- apps/functions/index.ts
+++ apps/functions/index.ts
@@ -1,6 +1,7 @@
 export * from './githubWebhook/index.js';
 export * from './ng-dev/index.js';
 export * from './code-of-conduct/index.js';
+export * from './dns-redirecting/index.js';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp();
