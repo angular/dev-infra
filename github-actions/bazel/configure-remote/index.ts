dcb9461dea5e275a08f02d6a9de3796dcf660e82
Joey Perrott <josephperrott@gmail.com>
2025-03-27 14:46:09 +0000

    feat(github-actions): allow a credential to be provided for configuring remote bazel setup
    Allow an input for a credential to provide authentication for RBE setup.

--- github-actions/bazel/configure-remote/index.ts
+++ github-actions/bazel/configure-remote/index.ts
@@ -6,34 +6,43 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-// @ts-ignore
+// @ts-ignore-next-line
 import tokenRaw from './gcp_token.data';
 import {k, iv, alg, at} from './constants.js';
 import {createDecipheriv} from 'crypto';
 import path from 'path';
 import fs from 'fs';
 import os from 'os';
-import {exportVariable} from '@actions/core';
+import {exportVariable, getBooleanInput, getInput} from '@actions/core';
 
-async function main(bazelRcPath: string | undefined) {
+async function main() {
   const isWindows = os.platform() === 'win32';
-  const t: Uint8Array = tokenRaw;
-  const dcip = createDecipheriv(alg, k, iv).setAuthTag(Buffer.from(at, 'base64'));
-  const dec = dcip.update(t, undefined, 'utf8') + dcip.final('utf8');
+  const bazelRcPath = getInput('bazelrc', {required: false, trimWhitespace: true});
+  const allowWindowsRbe = getBooleanInput('allow_windows_rbe', {required: true});
+  // If no credential is provided as an input, `getInput` will return an empty string
+  let credential = getInput('google_credential', {required: false, trimWhitespace: true}) || null;
+  // We treat any non-embedded credential as indication that this is a trusted build.
+  const trustedBuild = credential !== null;
+
+  if (credential === null) {
+    credential = getEmbeddedCredential();
+  }
 
   const destPath = isWindows
     ? path.join(process.env.APPDATA!, 'gcloud/application_default_credentials.json')
     : path.join(process.env.HOME!, '.config/gcloud/application_default_credentials.json');
 
   await fs.promises.mkdir(path.dirname(destPath), {recursive: true});
-  await fs.promises.writeFile(destPath, dec, 'utf8');
+  await fs.promises.writeFile(destPath, credential, 'utf8');
 
-  const allowWindowsRbe = process.env['ALLOW_WINDOWS_RBE'] === 'true';
   const configMode = isWindows && !allowWindowsRbe ? 'remote-cache' : 'remote';
 
   if (bazelRcPath) {
     let content = await readFileGracefully(bazelRcPath);
     content += `\nbuild --config=${configMode}`;
+    if (trustedBuild) {
+      content += `\nbuild --config=trusted-build`;
+    }
     await fs.promises.writeFile(bazelRcPath, content, 'utf8');
   }
 
@@ -50,7 +59,14 @@ async function readFileGracefully(filePath: string): Promise<string> {
   }
 }
 
-main(process.env.BAZELRC_PATH).catch((e) => {
+/** Extract the embeeded credential from the action. */
+function getEmbeddedCredential(): string {
+  const t: Uint8Array = tokenRaw;
+  const dcip = createDecipheriv(alg, k, iv).setAuthTag(Buffer.from(at, 'base64'));
+  return dcip.update(t, undefined, 'utf8') + dcip.final('utf8');
+}
+
+main().catch((e) => {
   console.error(e);
   process.exitCode = 1;
 });
