a90fd2879471a15a55c7e2fb4c600807a906072d
Joey Perrott <josephperrott@gmail.com>
2025-04-01 15:57:39 +0000

    refactor(github-actions): move to new key
    Move to new key for bazel remote

--- github-actions/bazel/configure-remote/constants.ts
+++ github-actions/bazel/configure-remote/constants.ts
@@ -9,6 +9,6 @@
 const owner = (process.env.CIRCLE_PROJECT_USERNAME ?? process.env.GITHUB_REPOSITORY_OWNER)!;
 
 export const alg = 'aes-256-gcm';
-export const at = 'bKddxrYADouso3haW7lCFA==';
+export const at = 'My6YOu2Le3+lG5WAHgQp8g==';
 export const k = owner.padEnd(32, '<');
 export const iv = '000003213213123213';
