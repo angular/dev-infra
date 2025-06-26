8d8bd82712686e66f48db22870c63116cdf79942
Joey Perrott <josephperrott@gmail.com>
2025-06-26 21:23:12 +0000

    fix(github-actions): reenable bundled token for remote configurations

--- github-actions/bazel/configure-remote/index.ts
+++ github-actions/bazel/configure-remote/index.ts
@@ -6,37 +6,45 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-// @ts-ignore
+// @ts-ignore-next-line
 import tokenRaw from './gcp_token.data';
 import {k, iv, alg, at} from './constants.js';
 import {createDecipheriv} from 'crypto';
 import path from 'path';
 import fs from 'fs';
 import os from 'os';
+import {exportVariable, getBooleanInput, getInput} from '@actions/core';
 
-async function main(bazelRcPath: string | undefined) {
+async function main() {
   const isWindows = os.platform() === 'win32';
-  const t: Uint8Array = tokenRaw;
-  const dcip = createDecipheriv(alg, k, iv).setAuthTag(Buffer.from(at, 'base64'));
-  const dec = dcip.update(t, undefined, 'utf8') + dcip.final('utf8');
+  const bazelRcPath = getInput('bazelrc', {required: false, trimWhitespace: true});
+  const allowWindowsRbe = getBooleanInput('allow_windows_rbe', {required: true});
+  const trustedBuild = getBooleanInput('trusted_build', {required: false});
+  const credential =
+    getInput('google_credential', {required: false, trimWhitespace: true}) ||
+    getEmbeddedCredential();
 
   const destPath = isWindows
     ? path.join(process.env.APPDATA!, 'gcloud/application_default_credentials.json')
     : path.join(process.env.HOME!, '.config/gcloud/application_default_credentials.json');
 
   await fs.promises.mkdir(path.dirname(destPath), {recursive: true});
-  await fs.promises.writeFile(destPath, dec, 'utf8');
+  await fs.promises.writeFile(destPath, credential, 'utf8');
+
+  const configMode = isWindows && !allowWindowsRbe ? 'remote-cache' : 'remote';
 
   if (bazelRcPath) {
     let content = await readFileGracefully(bazelRcPath);
-    if (isWindows) {
-      // Set the config to remote-cache as we do not have support for RBE on windows at this time
-      content += '\nbuild --config=remote-cache';
-    } else {
-      content += '\nbuild --config=remote';
+    content += `\nbuild --config=${configMode}`;
+    if (trustedBuild) {
+      content += `\nbuild --config=trusted-build`;
     }
     await fs.promises.writeFile(bazelRcPath, content, 'utf8');
   }
+
+  // Expose application credentials as variable. This may not be necessary with the default
+  // path being used for credentials, but it's helpful when we cross boundaries with e.g. WSL.
+  exportVariable('GOOGLE_APPLICATION_CREDENTIALS', destPath);
 }
 
 async function readFileGracefully(filePath: string): Promise<string> {
@@ -47,7 +55,14 @@ async function readFileGracefully(filePath: string): Promise<string> {
   }
 }
 
-main(process.env.BAZELRC_PATH).catch((e) => {
+/** Extract the embeeded credential from the action. */
+function getEmbeddedCredential(): string {
+  const t: Uint8Array = tokenRaw;
+  const dcip = createDecipheriv(alg, k, iv).setAuthTag(Buffer.from(at, 'base64'));
+  return dcip.update(t, undefined, 'utf8') + dcip.final('utf8');
+}
+
+main().catch((e) => {
   console.error(e);
   process.exitCode = 1;
 });
