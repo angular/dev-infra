09ff0631e93346a5929f101fce5f1562b8781c3c
Angular Robot <angular-robot@google.com>
2025-07-11 19:06:00 +0000

    build: update all non-major dependencies
    See associated pull request for more information.

--- github-actions/branch-manager/main.js
+++ github-actions/branch-manager/main.js
@@ -42877,7 +42877,7 @@ async function sendRequestWithRetries(state, request2, options, createdAt, retri
     return sendRequestWithRetries(state, request2, options, createdAt, retries);
   }
 }
-var VERSION12 = "8.0.1";
+var VERSION12 = "8.0.2";
 function createAppAuth(options) {
   if (!options.appId) {
     throw new Error("[@octokit/auth-app] appId option is required");
@@ -42890,12 +42890,10 @@ function createAppAuth(options) {
       "[@octokit/auth-app] installationId is set to a falsy value"
     );
   }
-  const log = Object.assign(
-    {
-      warn: console.warn.bind(console)
-    },
-    options.log
-  );
+  const log = options.log || {};
+  if (typeof log.warn !== "function") {
+    log.warn = console.warn.bind(console);
+  }
   const request2 = options.request || request.defaults({
     headers: {
       "user-agent": `octokit-auth-app.js/${VERSION12} ${getUserAgent()}`
