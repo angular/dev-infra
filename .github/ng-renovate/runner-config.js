3d092464fe48048f8e97e3e2291b5b648954e5ab
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-12 09:08:51 +0000

    build: allow scripts during renovate update
    This is needed as a workaround for PNPM due which causes the lock file to be broken due to Renovate passing `--ignore-pnpmfile`
    
    See: https://github.com/renovatebot/renovate/discussions/30812

--- .github/ng-renovate/runner-config.js
+++ .github/ng-renovate/runner-config.js
@@ -3,6 +3,8 @@ module.exports = {
   gitAuthor: 'Angular Robot <angular-robot@google.com>',
   platform: 'github',
   branchNameStrict: true,
+  // Temporary workaround for https://github.com/renovatebot/renovate/discussions/30812
+  allowScripts: true,
   // Renovate fork PRs should never be editable as Renovate would otherwise
   // not be able to delete the branches and future updates would be missed.
   forkModeDisallowMaintainerEdits: true,
