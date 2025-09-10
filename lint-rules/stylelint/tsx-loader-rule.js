f6a483e05f3b7db63ad212f19357c3e582bfd424
Joey Perrott <josephperrott@gmail.com>
2025-09-10 19:07:09 +0000

    build: remove ts-node
    Remove ts-node and its usages from the repo

--- lint-rules/stylelint/tsx-loader-rule.js
+++ lint-rules/stylelint/tsx-loader-rule.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const stylelint = require('stylelint');
 
-// Custom rule that registers all of the custom rules, written in TypeScript, with ts-node.
-require('ts-node').register();
+// Custom rule that registers all of the custom rules, written in TypeScript, with tsx.
+require('tsx/cjs/api').register();
 
 // Dummy rule so Stylelint doesn't complain that there aren't rules in the file.
 module.exports = stylelint.createPlugin('@angular/stylelint-ts-rule-loader', () => {});
