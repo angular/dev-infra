7d997a710d718335424aafa46ca0bbb362c76f90
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-10-09 10:29:52 +0000

    fixup! build: update remote execution environment

--- bazel/integration/tests/angular-cli/test/specs/example.e2e.ts
+++
@@ -1,7 +0,0 @@
-describe('start page', () => {
-  it('should work', async () => {
-    await browser.url(`http://localhost:4200`);
-
-    expect(await $('body').getText()).toContain('What do you want to do next with your app?');
-  });
-});
