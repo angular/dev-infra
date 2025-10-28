cad1e95f5ec9beb40232b420393c36735d4cd946
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-10-28 08:56:40 -0700

    feat(github-actions): add auto labeling by path to github labeler action
    This adds the ability to add additional labels automatically if a specific path is matched in the pull request commits.

--- ng-dev/pr/common/labels/managed.ts
+++ ng-dev/pr/common/labels/managed.ts
@@ -83,4 +83,10 @@ export const managedLabels = createTypedObject(ManagedLabel)({
     commitCheck: (c: Commit) => c.scope === 'localize',
     repositories: [ManagedRepositories.ANGULAR],
   },
+  DETECTED_REQUIRES_TGP: {
+    description: 'This PR requires a passing TGP before merging is allowed',
+    name: 'requires: TGP',
+    commitCheck: (c: Commit) => false,
+    repositories: [ManagedRepositories.ANGULAR],
+  },
 });
