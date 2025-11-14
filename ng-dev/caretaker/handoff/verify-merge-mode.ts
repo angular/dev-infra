aac826bdd556dba0be4b37c11aa862fca34f322a
Joey Perrott <josephperrott@gmail.com>
2025-11-14 19:00:20 +0000

    feat(ng-dev): add support for verifying merge-mode reset before caretaker handoff

---
+++ ng-dev/caretaker/handoff/verify-merge-mode.ts
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright Google LLC
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {RepositoryMergeModes} from '../../utils/config';
+import {bold, green, Log, red} from '../../utils/logging';
+import {Prompt} from '../../utils/prompt';
+import {getCurrentMergeMode, setRepoMergeMode} from '../../utils/git/repository-merge-mode';
+
+export async function verifyMergeMode(expectedMode: RepositoryMergeModes): Promise<boolean> {
+  const mode = await getCurrentMergeMode();
+  if (mode === expectedMode) {
+    return true;
+  }
+
+  Log.info(`The repository merge-mode is currently ${bold(mode)} and must be reset before handoff`);
+  if (
+    await Prompt.confirm({
+      message: `Would you like to reset this to ${expectedMode}`,
+      default: true,
+    })
+  ) {
+    try {
+      await setRepoMergeMode(expectedMode);
+      Log.info(`${green('✔')} Successfuly set merge-mode to ${expectedMode}`);
+      return true;
+    } catch (err) {
+      Log.info(`${red('✘')} Failed to update merge-mode`);
+      Log.info(err);
+      return false;
+    }
+  }
+  // User chose not to reset merge-mode
+  Log.info('Aborting...');
+  return false;
+}
