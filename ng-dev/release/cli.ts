bf74f3b99a289b7c4ba9d665ed5295da7800dba0
Joey Perrott <josephperrott@gmail.com>
2025-12-01 21:33:15 +0000

    fix(ng-dev): complete the removal of ng-dev release set-dist-tag as its no longer used

--- ng-dev/release/cli.ts
+++ ng-dev/release/cli.ts
@@ -12,7 +12,6 @@ import {ReleaseInfoCommandModule} from './info/cli.js';
 import {ReleaseNotesCommandModule} from './notes/cli.js';
 import {ReleasePrecheckCommandModule} from './precheck/cli.js';
 import {ReleasePublishCommandModule} from './publish/cli.js';
-import {ReleaseSetDistTagCommand} from './set-dist-tag/cli.js';
 import {BuildEnvStampCommand} from './stamping/cli.js';
 import {ReleaseNpmDistTagCommand} from './npm-dist-tag/cli.js';
 
@@ -27,7 +26,6 @@ export function buildReleaseParser(localYargs: Argv) {
     .command(ReleaseInfoCommandModule)
     .command(ReleaseNpmDistTagCommand)
     .command(ReleasePrecheckCommandModule)
-    .command(ReleaseSetDistTagCommand)
     .command(BuildEnvStampCommand)
     .command(ReleaseNotesCommandModule);
 }
--- ng-dev/release/set-dist-tag/README.md
+++
@@ -1,3 +0,0 @@
-This alias exists for backwards compatibility. Once the new `npm-dist-tag <set/|delete>`
-commands are available in most versions, this can be deleted and the publish tool external
-commands can be switched over.
