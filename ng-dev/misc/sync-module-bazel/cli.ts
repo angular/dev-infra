84e350c692e7ebf910118f413107c9dec2701d28
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2026-03-12 07:57:20 +0000

    fix(ng-dev): pass `moduleBazelPath` to `formatFiles` instead of literal filename
    Ensure the correct file is formatted.

--- ng-dev/misc/sync-module-bazel/cli.ts
+++ ng-dev/misc/sync-module-bazel/cli.ts
@@ -65,7 +65,7 @@ async function handler() {
   if (originalBazelContent !== moduleBazelContent) {
     writeFileSync(moduleBazelPath, moduleBazelContent);
 
-    await formatFiles(['MODULE.bazel']);
+    await formatFiles([moduleBazelPath]);
 
     ChildProcess.spawnSync('pnpm', ['bazel', 'mod', 'deps', '--lockfile_mode=update'], {
       suppressErrorOnFailingExitCode: true,
