05a55a0b0cf83d5f5be471f1d5af8dd60f8f6a88
Joey Perrott <josephperrott@gmail.com>
2026-06-05 23:01:21 +0000

    fix(bazel/rules_browser): prevent wildcard expansion in dmg conversion
    The script was previously expanding * directly in zip -r '../${OUTFILE}' *
    which allowed files starting with - to be interpreted as options by zip.
    This is fixed by adding -- before the wildcard to signal the end of options.
    
    Vulnerability: 6858cf88

--- bazel/rules/rules_browsers/browsers/private/convert_dmg_to_zip.sh
+++ bazel/rules/rules_browsers/browsers/private/convert_dmg_to_zip.sh
@@ -25,7 +25,7 @@ VOLUME=$(hdiutil attach "${DMGFILE}" | tail -1 | awk '{print $3}')
 cp -r "${VOLUME}/"*.app tmp
 hdiutil detach "${VOLUME}" >/dev/null
 cd tmp
-zip -r "../${OUTFILE}" *
+zip -r "../${OUTFILE}" -- *
 cd ..
 rm -rf tmp
 rm "${DMGFILE}"
